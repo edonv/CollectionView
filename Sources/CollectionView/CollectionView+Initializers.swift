4afa85b16f9241a67175c0c5ab18a402aed6cdc9
Edon <edon@valdman.works>
2023-09-27 15:09:32 -0500

    Update CollectionView+Initializers.swift
    Added extra initializer overloads to support single/no selection.

--- Sources/CollectionView/CollectionView+Initializers.swift
+++ Sources/CollectionView/CollectionView+Initializers.swift
@@ -25,9 +25,41 @@ extension CollectionView {
         }, backgroundConfiguration: nil, cellConfigurationHandler: cellConfigurationHandler)
     }
     
+    public init<Content>(
+        collection: Binding<ItemCollection>,
+        selection: Binding<Item?>?,
+        layout: CollectionLayout,
+        @ViewBuilder cellContent: @escaping (IndexPath, Item) -> Content,
+        cellConfigurationHandler: ((UICollectionViewCell, IndexPath, Item) -> Void)? = nil
+    ) where Content: View, ContentConfiguration == UIHostingConfiguration<Content, EmptyView> {
+        self.init(collection: collection, selection: selection, layout: layout, contentConfiguration: { indexPath, item in
+            UIHostingConfiguration {
+                cellContent(indexPath, item)
+            }
+        }, backgroundConfiguration: nil, cellConfigurationHandler: cellConfigurationHandler)
+    }
+    
     public init<Content, Background>(
         collection: Binding<ItemCollection>,
         selection: Binding<Set<Item>>,
+        layout: CollectionLayout,
+        @ViewBuilder cellContent: @escaping (IndexPath, Item) -> Content,
+        @ViewBuilder cellBackground: @escaping (IndexPath, Item) -> Background,
+        cellConfigurationHandler: ((UICollectionViewCell, IndexPath, Item) -> Void)? = nil
+    ) where Content: View, Background: View, ContentConfiguration == UIHostingConfiguration<Content, Background> {
+        self.init(collection: collection, selection: selection, layout: layout, contentConfiguration: { indexPath, item in
+            UIHostingConfiguration {
+                cellContent(indexPath, item)
+            }
+            .background {
+                cellBackground(indexPath, item)
+            }
+        }, backgroundConfiguration: nil, cellConfigurationHandler: cellConfigurationHandler)
+    }
+    
+    public init<Content, Background>(
+        collection: Binding<ItemCollection>,
+        selection: Binding<Item?>?,
         layout: CollectionLayout,
         @ViewBuilder cellContent: @escaping (IndexPath, Item) -> Content,
         @ViewBuilder cellBackground: @escaping (IndexPath, Item) -> Background,
@@ -58,6 +90,22 @@ extension CollectionView {
             .background(cellBackground)
         }, backgroundConfiguration: nil, cellConfigurationHandler: cellConfigurationHandler)
     }
+    
+    public init<Content, S>(
+        collection: Binding<ItemCollection>,
+        selection: Binding<Item?>?,
+        layout: CollectionLayout,
+        @ViewBuilder cellContent: @escaping (IndexPath, Item) -> Content,
+        cellBackground: S,
+        cellConfigurationHandler: ((UICollectionViewCell, IndexPath, Item) -> Void)? = nil
+    ) where Content: View, S: ShapeStyle, ContentConfiguration == UIHostingConfiguration<Content, _UIHostingConfigurationBackgroundView<S>> {
+        self.init(collection: collection, selection: selection, layout: layout, contentConfiguration: { indexPath, item in
+            UIHostingConfiguration {
+                cellContent(indexPath, item)
+            }
+            .background(cellBackground)
+        }, backgroundConfiguration: nil, cellConfigurationHandler: cellConfigurationHandler)
+    }
 }
 
 // MARK: - UICollectionLayoutListConfiguration
@@ -66,6 +114,26 @@ extension CollectionView where CollectionLayout == UICollectionViewCompositional
     public init(
         collection: Binding<ItemCollection>,
         selection: Binding<Set<Item>>,
+        listAppearance: UICollectionLayoutListConfiguration.Appearance,
+        listConfigurationHandler: ((_ config: inout UICollectionLayoutListConfiguration) -> Void)? = nil,
+        contentConfiguration: @escaping (IndexPath, Item) -> ContentConfiguration,
+        backgroundConfiguration: ((IndexPath, Item) -> UIBackgroundConfiguration)?,
+        cellConfigurationHandler: ((UICollectionViewCell, IndexPath, Item) -> Void)? = nil
+    ) {
+        var listConfig = UICollectionLayoutListConfiguration(appearance: listAppearance)
+        listConfigurationHandler?(&listConfig)
+        
+        self.init(collection: collection,
+                  selection: selection,
+                  layout: UICollectionViewCompositionalLayout.list(using: listConfig),
+                  contentConfiguration: contentConfiguration,
+                  backgroundConfiguration: backgroundConfiguration,
+                  cellConfigurationHandler: cellConfigurationHandler)
+    }
+    
+    public init(
+        collection: Binding<ItemCollection>,
+        selection: Binding<Item?>?,
         listAppearance: UICollectionLayoutListConfiguration.Appearance,
         listConfigurationHandler: ((_ config: inout UICollectionLayoutListConfiguration) -> Void)? = nil,
         contentConfiguration: @escaping (IndexPath, Item) -> ContentConfiguration,
