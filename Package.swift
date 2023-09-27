e046a5b13dca9db4ec8b0d31b26f4414eb3eac32
Edon <edon@valdman.works>
2023-09-27 09:47:18 -0500

    Update Package.swift
    Updated platform specifications.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "CollectionView",
+    platforms: [
+        .iOS(.v14),
+        .macCatalyst(.v14),
+        .tvOS(.v14),
+        .visionOS(.v1)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
