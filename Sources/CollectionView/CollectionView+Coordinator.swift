cc24e4449b6e33efa784a4764337b9468a54a434
Edon <edon@valdman.works>
2023-09-27 15:22:27 -0500

    Implemented UICollectionViewDelegate/view modifiers

--- Sources/CollectionView/CollectionView+Coordinator.swift
+++ Sources/CollectionView/CollectionView+Coordinator.swift
@@ -32,6 +32,96 @@ extension CollectionView {
             }
         }
         
+        // MARK: - UICollectionViewDelegate
+        
+        
+        
+        // MARK: Managing the selected cells
+        
+        public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+            parent.shouldSelectItemHandler?(collectionView, indexPath) ?? true
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+            parent.selection.insert(item)
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
+            parent.shouldDeselectItemHandler?(collectionView, indexPath) ?? true
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+            guard let item = dataSource.itemIdentifier(for: indexPath) else { return }
+            parent.selection.remove(item)
+        }
+        
+        // MARK: Multiple Selection Interaction
+        
+        public func collectionView(_ collectionView: UICollectionView, shouldBeginMultipleSelectionInteractionAt indexPath: IndexPath) -> Bool {
+            parent.shouldBeginMultipleSelectionInteractionHandler?(collectionView, indexPath) ?? false
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, didBeginMultipleSelectionInteractionAt indexPath: IndexPath) {
+            parent.didBeginMultipleSelectionInteractionHandler?(collectionView, indexPath)
+        }
+        
+        public func collectionViewDidEndMultipleSelectionInteraction(_ collectionView: UICollectionView) {
+            parent.didEndMultipleSelectionInteractionHandler?()
+        }
+        
+        // MARK: Managing cell highlighting
+        
+        public func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+            parent.shouldHighlightItemHandler?(collectionView, indexPath) ?? true
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+            parent.didHighlightItemHandler?(collectionView, indexPath)
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+            parent.didUnhighlightItemHandler?(collectionView, indexPath)
+        }
+        
+        // MARK: Tracking the addition and removal of views
+        
+        public func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+            parent.willDisplayCellHandler?(cell, indexPath)
+        }
+        
+//        public func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+//            <#code#>
+//        }
+        
+        public func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+            parent.didEndDisplayingCellHandler?(cell, indexPath)
+        }
+        
+//        public func collectionView(_ collectionView: UICollectionView, didEndDisplayingSupplementaryView view: UICollectionReusableView, forElementOfKind elementKind: String, at indexPath: IndexPath) {
+//            <#code#>
+//        }
+        
+        // MARK: Managing context menus
+        
+        public func collectionView(_ collectionView: UICollectionView, willDisplayContextMenu configuration: UIContextMenuConfiguration, animator: UIContextMenuInteractionAnimating?) {
+            parent.willDisplayContextMenu?(configuration, animator)
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, willEndContextMenuInteraction configuration: UIContextMenuConfiguration, animator: UIContextMenuInteractionAnimating?) {
+            parent.willEndContextMenuInteraction?(configuration, animator)
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, willPerformPreviewActionForMenuWith configuration: UIContextMenuConfiguration, animator: UIContextMenuInteractionCommitAnimating) {
+            parent.willPerformPreviewAction?(configuration, animator)
+        }
+        
+        public func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemsAt indexPaths: [IndexPath], point: CGPoint) -> UIContextMenuConfiguration? {
+            parent.contextMenuConfigHandler?(indexPaths, point)
+        }
+        
+        // TODO: implement collectionView(_:contextMenuConfiguration:highlightPreviewForItemAt:) and collectionView(_:contextMenuConfiguration:dismissalPreviewForItemAt:)
+        
         // MARK: - UICollectionViewDataSourcePrefetching
         
         public func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
--- Sources/CollectionView/CollectionView+ViewModifiers.swift
+++ Sources/CollectionView/CollectionView+ViewModifiers.swift
@@ -8,6 +8,179 @@
 import SwiftUI
 
 extension CollectionView {
+    // MARK: - Single Selection
+    
+    public func itemSelection(
+        shouldSelectItem: CollectionViewBoolCallback? = nil,
+        shouldDeselectItem: CollectionViewBoolCallback? = nil
+    ) -> CollectionView {
+        var new = self
+        new.shouldSelectItemHandler = shouldSelectItem
+        new.shouldDeselectItemHandler = shouldDeselectItem
+        return new
+    }
+    
+    public func shouldAllowSelection(
+        _ allowed: Bool
+    ) -> CollectionView {
+        var new = self
+        new.shouldSelectItemHandler = { _, _ in allowed }
+        return new
+    }
+    
+    // MARK: - Multiple Selection
+    
+    public func shouldBeginMultipleSelectionInteraction(
+        _ handler: CollectionViewBoolCallback?
+    ) -> CollectionView {
+        var new = self
+        new.shouldBeginMultipleSelectionInteractionHandler = handler
+        return new
+    }
+    
+    public func shouldAllowTwoFingerMultipleSelectionInteraction(
+        _ allowed: Bool
+    ) -> CollectionView {
+        var new = self
+        new.shouldBeginMultipleSelectionInteractionHandler = { _, _ in allowed }
+        return new
+    }
+    
+    public func didBeginMultipleSelectionInteraction(
+        _ handler: CollectionViewVoidCallback?
+    ) -> CollectionView {
+        var new = self
+        new.didBeginMultipleSelectionInteractionHandler = handler
+        return new
+    }
+    
+    public func didEndMultipleSelectionInteraction(
+        _ handler: (() -> Void)?
+    ) -> CollectionView {
+        var new = self
+        new.didEndMultipleSelectionInteractionHandler = handler
+        return new
+    }
+    
+    // MARK: - Highlighting
+    
+    public func shouldHighlightItem(
+        _ handler: CollectionViewBoolCallback?
+    ) -> CollectionView {
+        var new = self
+        new.shouldHighlightItemHandler = handler
+        return new
+    }
+    
+    public func shouldAllowHighlighting(
+        _ allowed: Bool
+    ) -> CollectionView {
+        var new = self
+        new.shouldHighlightItemHandler = { _, _ in allowed }
+        return new
+    }
+    
+    public func didHighlightItem(
+        _ handler: CollectionViewVoidCallback?
+    ) -> CollectionView {
+        var new = self
+        new.didHighlightItemHandler = handler
+        return new
+    }
+    
+    public func didUnhighlightItem(
+        _ handler: CollectionViewVoidCallback?
+    ) -> CollectionView {
+        var new = self
+        new.didUnhighlightItemHandler = handler
+        return new
+    }
+    
+    // MARK: - Displaying Cells
+    
+    public func willDisplayCell(
+        _ handler: ((_ cell: UICollectionViewCell, _ indexPath: IndexPath) -> Void)?
+    ) -> CollectionView {
+        var new = self
+        new.willDisplayCellHandler = handler
+        return new
+    }
+    
+    public func didEndDisplayingCell(
+        _ handler: ((_ cell: UICollectionViewCell, _ indexPath: IndexPath) -> Void)?
+    ) -> CollectionView {
+        var new = self
+        new.didEndDisplayingCellHandler = handler
+        return new
+    }
+    
+    // MARK: - Context Menu
+    
+    public func willDisplayContextMenu(
+        _ handler: ((_ configuration: UIContextMenuConfiguration, UIContextMenuInteractionAnimating?) -> Void)?
+    ) -> CollectionView {
+        var new = self
+        new.willDisplayContextMenu = handler
+        return new
+    }
+    
+    public func willEndContextMenuInteraction(
+        _ handler: ((_ configuration: UIContextMenuConfiguration, UIContextMenuInteractionAnimating?) -> Void)?
+    ) -> CollectionView {
+        var new = self
+        new.willEndContextMenuInteraction = handler
+        return new
+    }
+    
+    public func onContextMenuPreviewTap(
+        _ handler: ((_ configuration: UIContextMenuConfiguration, UIContextMenuInteractionAnimating?) -> Void)?
+    ) -> CollectionView {
+        var new = self
+        new.willPerformPreviewAction = handler
+        return new
+    }
+    
+    public func contextMenu<Preview: View>(
+        menuIdentifier: NSCopying? = nil,
+        previewProvider: ((_ indexPaths: [IndexPath], _ point: CGPoint) -> Preview)? = nil,
+        actionProvider: ((_ indexPaths: [IndexPath], _ point: CGPoint, [UIMenuElement]) -> UIMenu?)? = nil
+    ) -> CollectionView {
+        var new = self
+        new.contextMenuConfigHandler = { indexPaths, point in
+            UIContextMenuConfiguration(
+                identifier: menuIdentifier,
+                previewProvider: {
+                    guard let previewProvider else { return nil }
+                    return UIHostingController(rootView: previewProvider(indexPaths, point))
+                },
+                actionProvider: { menuElements in
+                    actionProvider?(indexPaths, point, menuElements)
+                }
+            )
+        }
+        return new
+    }
+    
+    public func contextMenu(
+        menuIdentifier: NSCopying? = nil,
+        previewProvider: ((_ indexPaths: [IndexPath], _ point: CGPoint) -> UIViewController)? = nil,
+        actionProvider: ((_ indexPaths: [IndexPath], _ point: CGPoint, [UIMenuElement]) -> UIMenu?)? = nil
+    ) -> CollectionView {
+        var new = self
+        new.contextMenuConfigHandler = { indexPaths, point in
+            UIContextMenuConfiguration(
+                identifier: menuIdentifier,
+                previewProvider: {
+                    previewProvider?(indexPaths, point)
+                },
+                actionProvider: { menuElements in
+                    actionProvider?(indexPaths, point, menuElements)
+                }
+            )
+        }
+        return new
+    }
+    
     // MARK: - Prefetching
     
     public func prefetching(
